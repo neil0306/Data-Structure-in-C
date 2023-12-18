2db94c8b3f5dcf11ce3c98e9223db7f988b2ddce
neil <719860478@qq.com>
2023-12-18 18:26:06 +0800

    图: 有向图的邻接矩阵; 网的邻接矩阵; 邻接矩阵的优劣.

---
+++ 5图/Lec_31/example_code/directed_graph.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct AMG_Graph      // Adjacency Matrix Graph
+{
+    int vex_num, edge_num;
+
+    char Vex[MAX];
+    int Edge[MAX][MAX];
+};
+
+struct AMG_Graph * create_AMG(void);
+void Show_AMG(struct AMG_Graph * graph);
+int search_vex(struct AMG_Graph * graph, char ch);
+
+int main(void)
+{
+    struct AMG_Graph * ud;      // 无向图指针
+    ud = create_AMG();
+    Show_AMG(ud);
+
+    return 0;
+}
+
+struct AMG_Graph * create_AMG(void)
+{
+    struct AMG_Graph * graph = NULL;
+    int i = 0, j = 0;
+    char u,v;
+
+    graph = (struct AMG_Graph *)malloc(sizeof(struct AMG_Graph));
+
+    printf("Please enter the number of vertex: ");
+    scanf("%d", &graph->vex_num);
+
+    printf("Please enter the number of edges: ");
+    scanf("%d", &graph->edge_num);
+
+    while(getchar() != '\n');           // 清掉回车
+    printf("Please enter the vertex:\n");
+    for(i = 0; i < graph->vex_num; i++){
+        graph->Vex[i] = getchar();
+        while(getchar() != '\n');       // 清掉回车
+    }
+
+    // 初始化二维数组, 防止垃圾信息影响结果
+    for(i = 0; i < graph->vex_num; i++){
+        for(j = 0; j < graph->vex_num; j++){
+            graph->Edge[i][j] = 0;
+        }
+    }
+
+    while(graph->edge_num--){           // 图有几条边, 就录入几次
+        printf("Please enter the vex that connect each other by edge:\n");
+        u = getchar();
+        while(getchar() != '\n');
+        v = getchar();
+        while(getchar() != '\n');
+
+        // 确认输入字符的节点编号
+        i = search_vex(graph, u);
+        j = search_vex(graph, v); 
+
+        if(i != -1 && j != -1){
+            graph->Edge[i][j] = 1;       // 无向图和有向图邻接矩阵代码的唯一区别
+        }
+        else{
+            printf("You've enter a wrong vex, please enter again.");
+            graph->edge_num++;      // 重置一下当前减掉的次数
+        }
+    }
+
+    return graph;
+}
+
+void Show_AMG(struct AMG_Graph * graph)
+{
+    int i,j;
+    printf("Show the vex: ");
+    for(i = 0; i < graph->vex_num; i++){
+        printf("%c", graph->Vex[i]);
+    }
+    printf("\n");
+
+    printf("Show the Adjacency matrices:\n");
+    for(i = 0; i < graph->vex_num; i++){
+        for(j = 0; j < graph->vex_num; j++){
+            printf("%d\t", graph->Edge[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int search_vex(struct AMG_Graph * graph, char ch)
+{
+    int i;
+    for(i = 0; i < graph->vex_num; i++){
+        if (ch == graph->Vex[i]){
+            return i;
+        }
+    }
+    return -1;
+}

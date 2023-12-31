fbb8f1c50b92a7beb031f7b2b5fbd9cf3401be30
neil <719860478@qq.com>
2023-12-31 15:39:35 +0800

    排序: 堆排序--建立初始堆

---
+++ 数据结构--排序/example_code/HeapSort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#define SIZE 10 
+
+void HeapkSort(int array[], int n);     // n 是数组最大下标
+void Sink(int array[], int k, int n);   // 将节点k进行下沉(k是子二叉树根节点), n表示数组最大下标
+void Swap(int * a, int *b);             // 交换节点元素
+
+
+int main(void)
+{
+    int arr[SIZE] = {13, 4, 18, 31, 29, 12, 18, 22, 8, 21};
+
+    printf("Original array\t: ");
+    for (int i = 0; i < SIZE; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // 排序
+    HeapkSort(arr, SIZE-1);
+
+    printf("After sort\t: ");
+    for (int i = 0; i < SIZE; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+void HeapkSort(int array[], int n)
+{
+    for(int i = (n-1)/2; i >=0; i--){
+        Sink(array, i, n);          // 对每一个遍历的节点都调用下沉函数(只有满足条件的节点才会下沉)
+    }
+}
+
+void Sink(int array[], int k, int n)
+{
+    while( 2*k + 1 <= n){                       // 检查有没有左子树, 如有, 则进入循环
+        int j = 2*k + 1;                        // j此时表示当前遍历到的左子树节点
+
+        // 找左右子树中最大的节点下标
+        if( j < n && array[j] < array[j+1]){    // j < n 说明一定有右子树; 右子树比左子树大
+            j++;                                // j此时改为右子树节点
+        }
+
+        // 根节点与左右子树最大者进行比较
+        if(array[k] > array[j]){                // 根节点比左右子树都大, 此时不需要下沉
+            break;
+        }
+        else{
+            Swap(&array[k], &array[j]);         // 交换节点元素, 注意传入的是地址! 
+        }
+
+        // 更新当前遍历的根节点
+        k = j;
+    }
+}
+
+void Swap(int * a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
